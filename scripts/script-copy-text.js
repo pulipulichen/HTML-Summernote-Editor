8ebb894f0b295263d10efc792657188499253656
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-31 20:26:52 +0800

    Wed May 31 08:26:52 PM CST 2023

--- scripts/script-copy-text.js
+++ scripts/script-copy-text.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
     let markdown = getMarkdown()
 
     let text = markdown
+    console.log(text)
     while (text.indexOf('\n\n') > -1) {
       text = text.replace(/\n\n/g, '\n')
+    }
+    while (text.indexOf('\\[') > -1) {
+      text = text.replace(/\\\[/g, '[')
+    }
+    while (text.indexOf('\\]') > -1) {
+      text = text.replace(/\\\]/g, ']')
     }
     text = text.replace(/\*   /g, '\t')
     text = text.replace(/    /g, '\t')
