92e33f7886c70da8c76855354cf09c5e21ec3ce2
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-09-09 16:46:22 +0800

    西元2023年09月09日 (週六) 16時46分22秒 CST

--- scripts/script-summernote.js
+++ scripts/script-summernote.js
@@ -44,4 +44,45 @@ if (code) {
   document.title = replace
 }
 
+$('#summernote').on('summernote.paste', function(fn, e) {
+  // console.log('ok')
+  // Check if the pasted content looks like HTML code
+  if (!e) {
+    return false
+  }
+  if (e.originalEvent) {
+    e = e.originalEvent
+  }
+  if (!e.clipboardData) {
+    return false
+  }
+  
+
+  const pastedContent = (e.originalEvent || e).clipboardData.getData('text/plain');
+  // console.log(pastedContent)
+  if (isHTML(pastedContent)) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    // Set Summernote to code view and insert the HTML code
+    // $('#summernote').summernote('codeview.activate');
+    $('#summernote').summernote('code', pastedContent);
+
+    // Restore cursor position to the end of the inserted code
+    // setTimeout(function() {
+    //   $('#summernote').summernote('restoreRange', savedRange);
+    // }, 0);
+    
+    // Switch back to WYSIWYG mode after a brief delay
+    // setTimeout(function() {
+    //   $('#summernote').summernote('codeview.deactivate');
+    // }, 100);
+  }
+});
+
+function isHTML(content) {
+  const htmlRegex = /<([A-Z][A-Z0-9]*)\b[^>]*>(.*?)<\/\1>/i;
+  return htmlRegex.test(content);
+}
+
 });
